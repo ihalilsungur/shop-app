a1ed881947d8de51d2cb679501e5b4dcb01c07a6
ibrahim halil <ihalilsungur@gmail.com>
2021-09-05 11:57:24 +0300

    projemizi olusturduk

---
+++ data.js
@@ -0,0 +1,20 @@
+module.exports = function () {
+  return {
+    products: [
+      {id:1,name:"Samasung S5",price:1000,imageUrl:"1.jpeg",description:"İdare eder."},
+      {id:2,name:"Samasung S6",price:2000,imageUrl:"2.jpeg",description:"İdare eder."},
+      {id:3,name:"Samasung S7",price:3000,imageUrl:"3.jpeg",description:"İdare eder."},
+      {id:4,name:"Samasung S8",price:4000,imageUrl:"4.jpeg",description:"İdare eder."},
+      {id:5,name:"Samasung S9",price:5000,imageUrl:"5.jpeg",description:"İdare eder."},
+      {id:6,name:"Samasung S10",price:6000,imageUrl:"6.jpeg",description:"İdare eder."},
+      {id:7,name:"Samasung S11",price:7000,imageUrl:"7.jpeg",description:"İdare eder."},
+      {id:8,name:"Samasung S12",price:8000,imageUrl:"8.jpeg",description:"İdare eder."}
+    ],
+    categories: [
+      {id:1,name:"Telefon"},
+      {id:2,name:"Bilgisayar"},
+      {id:3,name:"Elektronik"}
+    ],
+    orders: [],
+  };
+};
